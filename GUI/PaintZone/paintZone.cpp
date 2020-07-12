4f703bb0948ad6b987c26068b2c4923bcb4edc55
erhuo527 <59333755+erhuo527@users.noreply.github.com>
2020-07-12 22:30:50 +0800

    Add files via upload

--- GUI/PaintZone/paintZone.cpp
+++ GUI/PaintZone/paintZone.cpp
@@ -1,24 +1,48 @@
-/*
- * @Description: 
- * @Author: szq
- * @Github: https://github.com/MrQqqq
- * @Date: 2020-07-11 15:59:50
- * @LastEditors: szq
- * @LastEditTime: 2020-07-11 17:26:10
- * @FilePath: \QTCreateByCMake\GUI\PaintZone\paintZone.cpp
- */ 
+//#pragma execution_character_set("utf-8")   //解决中文乱码问题
+
 #include "paintZone.h"
-#include <QTextEdit>
+#include "lightCircle.h"
+#include "ui_paintZone.h"
+#include "controlStyle.h"
+#include "qdesktopwidget.h"
+#include<QDebug>
+#include<QIcon>
+
 CPaintZone::CPaintZone(QWidget *parent) :
-    QMainWindow(parent)
+    QWidget(parent),
+    ui(new Ui::CPaintZone)
 {
-    
-    QTextEdit *ptr_textArea = new QTextEdit(this);
-    ptr_textArea->setText("Widget");
-    setCentralWidget(ptr_textArea);
+    ui->setupUi(this);
+    this->initForm();
 }
 
 CPaintZone::~CPaintZone()
 {
-    
+    delete ui;
+}
+
+void CPaintZone::initForm()
+{
+    this->setStyleSheet("*{outline:0px;}QWidget#lightControl{background:72777b;}");
+
+    controlStyle::setSliderQss(ui->light_brightness, 10, "#E8EDF2", "#3498DB", "#3498DB");
+
+    lightCircle* lcp = new lightCircle(ui->lightCirclePanel);
+    lcp->setFixedSize(QSize(171,171));
+
+//  controlStyle::setPushButtonQss(ui->light_source, 50, 0, "#3498DB", "#FFFFFF", "#5DACE4", "#E5FEFF", "#2483C7", "#A0DAFB");
+
+    ui->light_on->setIcon(QIcon(":icons/light_on.png"));
+    ui->light_on->setFixedSize(QSize(22,22));
+    ui->light_off->setIcon(QIcon(":icons/light_off.png"));
+    ui->light_off->setFixedSize(QSize(22,22));
+    ui->light_mod_1->setIcon(QIcon(":icons/light_mod_1.png"));
+    ui->light_mod_1->setFixedSize(QSize(22,22));
+    ui->light_mod_2->setIcon(QIcon(":icons/light_mod_2.png"));
+    ui->light_mod_2->setFixedSize(QSize(22,22));
+    ui->light_mod_3->setIcon(QIcon(":icons/light_mod_3.png"));
+    ui->light_mod_3->setFixedSize(QSize(22,22));
+    ui->light_mod_4->setIcon(QIcon(":icons/light_mod_4.png"));
+    ui->light_mod_4->setFixedSize(QSize(22,22));
+
 }
--- GUI/PaintZone/paintZone.h
+++ GUI/PaintZone/paintZone.h
@@ -1,23 +1,25 @@
-/*
- * @Description: 
- * @Author: szq
- * @Github: https://github.com/MrQqqq
- * @Date: 2020-07-11 15:59:26
- * @LastEditors: szq
- * @LastEditTime: 2020-07-11 16:32:45
- * @FilePath: \QTCreateByCMake\GUI\PaintZone\operationZoneRight.h
- */ 
 #ifndef CPaintZone_H
 #define CPaintZone_H
 
-#include <QMainWindow>
-class CPaintZone : public QMainWindow
+#include <QWidget>
+
+namespace Ui {
+class CPaintZone;
+}
+
+class CPaintZone : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit CPaintZone(QWidget *parent = 0);
     ~CPaintZone();
+
+private:
+    Ui::CPaintZone *ui;
+
+private slots:
+    void initForm();
 };
 
-#endif // CPaintZone_H
+#endif // LIGHTCONTROL_H
