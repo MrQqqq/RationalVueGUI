dd561bfa282e52fad881054df5908a30998ffc3c
734413482 <43592014+734413482@users.noreply.github.com>
2020-07-12 19:26:22 +0800

    Add files via upload

--- GUI/DataZoneRight/dataZoneRight.h
+++ GUI/DataZoneRight/dataZoneRight.h
@@ -1,23 +1,43 @@
-/*
- * @Description: 
- * @Author: szq
- * @Github: https://github.com/MrQqqq
- * @Date: 2020-07-11 15:59:26
- * @LastEditors: szq
- * @LastEditTime: 2020-07-11 17:24:26
- * @FilePath: \QTCreateByCMake\GUI\DataZoneRight\dataZoneRight.h
- */ 
 #ifndef DATAZONERIGHT_H
 #define DATAZONERIGHT_H
 
 #include <QMainWindow>
+#include <qmenu.h>
+#include <qaction.h>
+#include <QTreeWidgetItem>
+#include <QContextMenuEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class CdataZoneRight; }
+QT_END_NAMESPACE
+
 class CDataZoneRight : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit CDataZoneRight(QWidget *parent = 0);
+	CDataZoneRight(QWidget *parent = nullptr);
     ~CDataZoneRight();
-};
 
-#endif // DATAZONERIGHT_H
+    void initTreeWidget();
+
+    void createMenu();
+
+protected:
+    void contextMenuEvent(QContextMenuEvent *event);
+
+private slots:
+    void onDelAction();
+    void onAddAction();
+
+private:
+    Ui::CdataZoneRight *ui;
+
+    QMenu *m_PopMenu;
+    QMenu *m_AddMenu;
+    QMenu *m_DelMenu;
+
+    QAction *m_AddAction;
+    QAction *m_DelAction;
+};
+#endif // CDATAZONELEFT_H
