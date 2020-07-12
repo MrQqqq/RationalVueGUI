5cc722ff128ff3b9e42d579d1fb87c1af46d95c2
romizzz <46618825+romizzz@users.noreply.github.com>
2020-07-12 19:56:56 +0800

    Add files via upload

--- GUI/DataZoneLeft/dataZoneLeft.h
+++ GUI/DataZoneLeft/dataZoneLeft.h
@@ -1,23 +1,69 @@
-/*
- * @Description: 
- * @Author: szq
- * @Github: https://github.com/MrQqqq
- * @Date: 2020-07-11 15:59:26
- * @LastEditors: szq
- * @LastEditTime: 2020-07-11 17:23:52
- * @FilePath: \QTCreateByCMake\GUI\DataZoneLeft\dataZoneLeft.h
- */ 
-#ifndef DATAZONELEFT_H
-#define DATAZONELEFT_H
+#ifndef CDATAZONELEFT_H
+#define CDATAZONELEFT_H
 
-#include <QMainWindow>
-class CDataZoneLeft : public QMainWindow
+#include <QWidget>
+
+#include<QDockWidget>
+#include<QDockWidget>
+#include<QWidget>
+#include<QVBoxLayout>
+#include<QString>
+#include<QIcon>
+#include<QToolButton>
+#include<QCheckBox>
+#include<QListWidget>
+#include<QListWidgetItem>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class CDataZoneLeft; }
+QT_END_NAMESPACE
+
+class CDataZoneLeft : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit CDataZoneLeft(QWidget *parent = 0);
+    CDataZoneLeft(QWidget *parent = nullptr);
     ~CDataZoneLeft();
-};
 
-#endif // DATAZONELEFT_H
+    QVBoxLayout *m_pVBoxLayout;
+    QVBoxLayout *m_pVBoxLayout_2;
+
+    QHBoxLayout *m_pHBoxLayout;
+
+    QToolButton *m_pToolButton;
+    QToolButton *m_pToolButton_2;
+    QToolButton *m_pToolButton_3;
+    QToolButton *m_pToolButton_4;
+    QToolButton *m_pToolButton_5;
+    QToolButton *m_pToolButton_6;
+    QToolButton *m_pToolButton_7;
+    QToolButton *m_pToolButton_8;
+    QToolButton *m_pToolButton_9;
+
+    QCheckBox *m_pCheckBox;
+    QCheckBox *m_pCheckBox_2;
+    QCheckBox *m_pCheckBox_3;
+    QCheckBox *m_pCheckBox_4;
+    QCheckBox *m_pCheckBox_5;
+    QCheckBox *m_pCheckBox_6;
+    QCheckBox *m_pCheckBox_7;
+    QCheckBox *m_pCheckBox_8;
+    QCheckBox *m_pCheckBox_9;
+
+    QListWidgetItem * m_pItem;
+    QListWidgetItem * m_pItem_2;
+    QListWidgetItem * m_pItem_3;
+    QListWidgetItem * m_pItem_4;
+    QListWidgetItem * m_pItem_5;
+    QListWidgetItem * m_pItem_6;
+    QListWidgetItem * m_pItem_7;
+    QListWidgetItem * m_pItem_8;
+    QListWidgetItem * m_pItem_9;
+
+
+private:
+    Ui::CDataZoneLeft *ui;
+};
+#endif // CDATAZONELEFT_H
